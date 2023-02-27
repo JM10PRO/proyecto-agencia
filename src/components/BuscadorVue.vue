e10d7bb0ed0c4a6ac98ec9b75eb0bc54ee49192f
José María <josemariagl99@gmail.com>
2023-02-27 18:40:14 +0100

    vue2-datapicker en buscador component

--- src/components/BuscadorVue.vue
+++ src/components/BuscadorVue.vue
@@ -22,12 +22,17 @@
                             <div class="row p-2">
                                 <div class="col">
                                     <label for="fecha1" class="form-label me-2 fw-semibold">Fecha 1:</label>
-                                    <input type="text" name="fecha1" id="fecha1">
+                                    <date-picker v-model="fecha1" valueType="format" name="fecha1" id="fecha1"></date-picker>
                                 </div>
                                 <div class="col">
                                     <label for="fecha2" class="form-label me-2 fw-semibold">Fecha 2:</label>
-                                    <input type="date" name="fecha2" id="fecha2" class="form-control-md">
+                                    <date-picker v-model="fecha2" valueType="format" name="fecha2" id="fecha2"></date-picker>
                                 </div>    
+                            </div>
+                            <div class="row p-2">
+                                <div class="col">
+                                    <button @click="buscar" type="button" class="btn btn-outline-success">Buscar</button>
+                                </div>
                             </div>
                         </div>
                         <div class="col col-3 px-5">
@@ -44,29 +49,35 @@
                     </div>
                 </div>
             </div>
-            <div class="table-responsive rounded-3">
-                <table class="table table-primary">
-                    <thead>
-                        <tr>
-                            <th scope="col">Column 1</th>
-                            <th scope="col">Column 2</th>
-                            <th scope="col">Column 3</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="">
-                            <td scope="row">R1C1</td>
-                            <td>R1C2</td>
-                            <td>R1C3</td>
-                        </tr>
-                        <tr class="">
-                            <td scope="row">Item</td>
-                            <td>Item</td>
-                            <td>Item</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div v-if="resultado" class="card">
+                <div class="card-header bg-success-subtle">
+                    <p class="h5 fw-semibold mt-1">Resultados de la búsqueda</p>
+                </div>
+                <div class="table-responsive bg-primary-subtle">
+                    <table class="table table-primary mb-1">
+                        <thead>
+                            <tr>
+                                <th scope="col">Estamos</th>
+                                <th scope="col">en</th>
+                                <th scope="col">mantenimiento</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="">
+                                <td scope="row">R1C1</td>
+                                <td>R1C2</td>
+                                <td>R1C3</td>
+                            </tr>
+                            <tr class="">
+                                <td scope="row">Item</td>
+                                <td>Item</td>
+                                <td>Item</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>    
             </div>
+            
             
         </div>
     </div>
@@ -74,16 +85,37 @@
 <script>
 
 import NavBar from './NavBar.vue'
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+import 'vue2-datepicker/locale/es';
 
 export default {
-    created() {
-        this.datePicker();
-    },
     components:{
         NavBar,
+        DatePicker,
+    },
+    data() {
+        return {
+            cod1: null,
+            cod2: null,
+            fecha1: null,
+            fecha2: null,
+            resultado: false
+        }
     },
     methods: {
-      
+        buscar() {
+            this.resultado = true;
+            // fetch("http://localhost/agencia-seguros/php/polizas/?consultardatospoliza=" + this.$route.params.numero)
+            //     .then((respuesta) => respuesta.json())
+            //     .then((datosRespuesta) => {
+            //     console.log(datosRespuesta);
+            //     this.poliza = datosRespuesta[0];
+            //     })
+            //     .catch(console.log);
+            // },
+        },
+    
     },
 }
 </script>
